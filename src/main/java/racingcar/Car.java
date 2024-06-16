3945700752f715512eba541cc65be1f367692713
seongdong <chosd1123@naver.com>
2024-06-16 17:13:42 +0900

    read README

--- src/main/java/racingcar/Car.java
+++ src/main/java/racingcar/Car.java
@@ -1,8 +1,20 @@
 package racingcar;
 
+import static camp.nextstep.edu.missionutils.Randoms.pickNumberInRange;
+import static camp.nextstep.edu.missionutils.Console.readLine;
+
+
 public class Car {
-    private final String name;
+    //private final String name;
     private int position = 0;
+
+    private void init(){
+
+    }
+
+    public Car(){
+        init();
+    }
 
     public Car(String name) {
         this.name = name;
